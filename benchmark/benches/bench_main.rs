58f6d751ff4cddada8f4588d6d6406c737158e32
ikanago <ikanago-dev@protonmail.com>
2021-12-13 16:31:26 +0900

    Initialize benchmark workspace

---
+++ benchmark/benches/bench_main.rs
@@ -0,0 +1,7 @@
+use criterion::criterion_main;
+
+mod sync_naive_lru;
+
+criterion_main!{
+    sync_naive_lru::sync_naive_lru_benches,
+}
---
+++ benchmark/benches/sync_naive_lru.rs
@@ -0,0 +1,13 @@
+use criterion::{criterion_group, Criterion};
+use sync_naive_lru::SyncNaiveLru;
+
+fn insert() {
+    let mut lru = SyncNaiveLru::new(4);
+    lru.insert(1, 2);
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("insert", |b| b.iter(|| insert()));
+}
+
+criterion_group!(sync_naive_lru_benches, criterion_benchmark);
